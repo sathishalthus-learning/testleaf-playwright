fdf275b08649ddc98f99a5d5c28b24369d34c72b
sathishalthus-learning <sathishalthus.learning@gmail.com>
2025-02-05 00:26:14 +0530

    clean up

--- tests/01-launch-browser.spec.ts
+++
@@ -1,9 +0,0 @@
-import { chromium,test } from '@playwright/test';
-
-test('launch browser', async () => {
-  const browser = await chromium.launch();
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.goto('https://google.co.in');
-  console.log(await page.title());
-});
